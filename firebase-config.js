febbd47d485b6cf77170aa783dc9e115fb5d3248
ahmedrashed90 <134849433+ahmedrashed90@users.noreply.github.com>
2026-05-18 17:18:37 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -12,7 +12,7 @@ window.MZJ_FIREBASE_CONFIG = {
 // عند ربط Firebase SDK لاحقًا، استخدم window.MZJ_FIREBASE_CONFIG كمصدر واحد للكونفيج.
 
 // ضع رابط Google Apps Script Web App هنا بعد نشر الاسكربت.
-window.MZJ_DRIVE_UPLOAD_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwB97g1inNBWcInSsHoUj28i7V0rb-BHxesSeZfWMEsDfYp4nF0MAb4WkaNe1Xq7AIi/exec";
+window.MZJ_DRIVE_UPLOAD_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxzeV9xA8cHEf1mkYHJtWQZRxVLzPGVTTQ97XdKsg-qvhOzKdOv6sZHit0cwzoFjRxS/exec";
 
 // Firebase config - MZJ stock readonly database
 window.MZJ_STOCK_FIREBASE_CONFIG = {
